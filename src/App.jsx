d0725c96984ef08efb6358cba9976c7411fa0e62
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-07 17:03:58 -0600

    Creating a table

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
 
-  return App(
-    // 
+  return (
+     <div>
+      <h2>. . . CALCULATOR . . .</h2>
+      <br />
+      {/* TABLE */}
+      <table style={{border:'2px solid black', width:'400px'}}>
+        {/* ROW */}
+        <tr>
+          <td align="center">RESULT</td>
+        </tr>
+        {/* END ROW */}
+      </table>
+      {/* END TABLE */}
+     </div>
   )
 }
 
