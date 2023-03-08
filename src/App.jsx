d20339170012b93a1a7aa4d1dfd806b5c2a3f349
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-08 15:53:36 -0600

    Adding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,54 @@ function App() {
      <div align="center">
       <h3>. . . CALCULATOR . . .</h3>
       <br />
-
       {/* START TABLE */}
       <table style={{ border: '5px solid black', width: '450px' }}>
         {/* FIRST ROW */}
         <tr style={{ border: '2px solid black', height:'60px'}}>
-          <td colSpan={4} align="center" style={{ border: '2px solid black' }}><h5>RESULT</h5></td> {/* Column */}
+          <td colSpan={4} align="center" style={{ border: '2px solid black' }}>
+            <h5>RESULT</h5>
+            </td> {/* Column */}
         </tr>
+
         {/* SECOND ROW */}
         <tr style={{ border: '2px solid black', height:'60px' } }>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><h4>/</h4></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><h4>*</h4></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><h4>__</h4></td>
-          <td rowSpan={2} align="center" style={{ border: '2px solid black', width:'30px' }}><h1>+</h1></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>/</h4></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>*</h4></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>-</h4></button></td>
+          <td rowSpan={2} align="center" style={{ border: '2px solid black', width: '30px' }}><button><h1>+</h1></button></td>
         </tr>
+        
         {/* THIRD ROW */}
         <tr style={{ border: '2px solid black', height:'60px' }}>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><b>7</b></td>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><b>8</b></td>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><b>9</b></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>7</b></button></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>8</b></button></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>9</b></button></td>
         </tr>
+        
         {/* FOURTH ROW */}
         <tr style={{ border: '2px solid black', height:'60px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><b>4</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>5</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>6</b></td>
-          <td rowSpan={3} align="center" style={{ border: '2px solid black' }}><h1>=</h1></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>4</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>5</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>6</b></button></td>
+          <td rowSpan={3} align="center" style={{ border: '2px solid black' }}><button><h1>=</h1></button></td>
         </tr>
+        
         {/* FIFTH ROW */}
         <tr style={{ border: '2px solid black', height:'60px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><b>1</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>2</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>3</b></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>1</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>2</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>3</b></button></td>
         </tr>
+        
         {/* SIXTH ROW */}
         <tr style={{ border: '2px solid black', height:'60px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><b>C</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>0</b></td>
-          <td align="center" style={{ border: '2px solid black' }}><b>Del</b></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>C</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>0</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button><b>Del</b></button></td>
         </tr>
-      </table> {/* END TABLE */}
+
+      </table>
+      {/* END TABLE */}
      </div>
   )
 }
