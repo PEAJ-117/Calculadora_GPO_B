8adacf6640bc644a9750cf6e72d59622ba473447
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-08 16:04:40 -0600

    Updating table size

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,31 @@ function App() {
       <h3>. . . CALCULATOR . . .</h3>
       <br />
       {/* START TABLE */}
-      <table style={{ border: '5px solid black', width: '450px' }}>
+      <table style={{ border: '5px solid black', width: '250px' }}>
         {/* FIRST ROW */}
-        <tr style={{ border: '2px solid black', height:'60px'}}>
+        <tr style={{ border: '2px solid black', height:'45px'}}>
           <td colSpan={4} align="center" style={{ border: '2px solid black' }}>
             <h5>RESULT</h5>
             </td> {/* Column */}
         </tr>
 
         {/* SECOND ROW */}
-        <tr style={{ border: '2px solid black', height:'60px' } }>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>/</h4></button></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>*</h4></button></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><h4>-</h4></button></td>
+        <tr style={{ border: '2px solid black', height:'45px' } }>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>/</b></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>*</b></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>-</b></button></td>
           <td rowSpan={2} align="center" style={{ border: '2px solid black', width: '30px' }}><button><h1>+</h1></button></td>
         </tr>
         
         {/* THIRD ROW */}
-        <tr style={{ border: '2px solid black', height:'60px' }}>
+        <tr style={{ border: '2px solid black', height:'45px' }}>
           <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>7</b></button></td>
           <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>8</b></button></td>
           <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>9</b></button></td>
         </tr>
         
         {/* FOURTH ROW */}
-        <tr style={{ border: '2px solid black', height:'60px' }}>
+        <tr style={{ border: '2px solid black', height:'45px' }}>
           <td align="center" style={{ border: '2px solid black' }}><button><b>4</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>5</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>6</b></button></td>
@@ -37,14 +37,14 @@ function App() {
         </tr>
         
         {/* FIFTH ROW */}
-        <tr style={{ border: '2px solid black', height:'60px' }}>
+        <tr style={{ border: '2px solid black', height:'45px' }}>
           <td align="center" style={{ border: '2px solid black' }}><button><b>1</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>2</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>3</b></button></td>
         </tr>
         
         {/* SIXTH ROW */}
-        <tr style={{ border: '2px solid black', height:'60px' }}>
+        <tr style={{ border: '2px solid black', height:'45px' }}>
           <td align="center" style={{ border: '2px solid black' }}><button><b>C</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>0</b></button></td>
           <td align="center" style={{ border: '2px solid black' }}><button><b>Del</b></button></td>
