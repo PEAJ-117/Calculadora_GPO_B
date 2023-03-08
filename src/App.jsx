039f91a937bb691e8552f8f1bd427ad7e1de448a
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-08 09:34:56 -0600

    Adding Rows & Columns

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,52 @@ function App() {
 
   return (
      <div align="center">
-      <h2>. . . CALCULATOR . . .</h2>
+      <h3>. . . CALCULATOR . . .</h3>
       <br />
 
       {/* START TABLE */}
-      <table style={{ border: '5px solid black', width: '600px' }}>
+      <table style={{ border: '5px solid black', width: '450px' }}>
         {/* FIRST ROW */}
-        <tr style={{ border: '2px solid black'}}>
-          <td align="center">Fila 1</td>
+        <tr style={{ border: '2px solid black', height:'60px'}}>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 1, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 1, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 1, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 1, Col 4</td>
         </tr>
         {/* SECOND ROW */}
-        <tr style={{ border: '2px solid black' } }>
-          <td align="center">Fila 2</td>
+        <tr style={{ border: '2px solid black', height:'60px' } }>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 2, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 2, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 2, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 2, Col 4</td>
         </tr>
         {/* THIRD ROW */}
-        <tr style={{ border: '2px solid black' }}>
-          <td align="center">Fila 3</td>
+        <tr style={{ border: '2px solid black', height:'60px' }}>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 3, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 3, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 3, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 3, Col 4</td>
         </tr>
         {/* FOURTH ROW */}
-        <tr style={{ border: '2px solid black' }}>
-          <td align="center">Fila 4</td>
+        <tr style={{ border: '2px solid black', height:'60px' }}>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 4, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 4, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 4, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 4, Col 4</td>
         </tr>
         {/* FIFTH ROW */}
-        <tr style={{ border: '2px solid black' }}>
-          <td align="center">Fila 5</td>
+        <tr style={{ border: '2px solid black', height:'60px' }}>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 5, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 5, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 5, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 5, Col 4</td>
         </tr>
         {/* SIXTH ROW */}
-        <tr style={{ border: '2px solid black' }}>
-          <td align="center">Fila 6</td>
+        <tr style={{ border: '2px solid black', height:'60px' }}>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 6, Col 1</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 6, Col 2</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 6, Col 3</td>
+          <td align="center" style={{ border: '2px solid black' }}>Fila 6, Col 4</td>
         </tr>
       </table> {/* END TABLE */}
      </div>
