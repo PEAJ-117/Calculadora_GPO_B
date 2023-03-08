8e24e2ce153d3690d3d7a31ec116815ee46359fa
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-08 16:21:04 -0600

    Styles of the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -15,39 +15,39 @@ function App() {
 
         {/* SECOND ROW */}
         <tr style={{ border: '2px solid black', height:'45px' } }>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>/</b></button></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>*</b></button></td>
-          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button><b>-</b></button></td>
-          <td rowSpan={2} align="center" style={{ border: '2px solid black', width: '30px' }}><button><h1>+</h1></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button type="button" className="btn btn-success"><b>/</b></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button type="button" className="btn btn-success"><b>*</b></button></td>
+          <td align="center" style={{ border: '2px solid black', width:'30px' }}><button type="button" className="btn btn-success"><b>-</b></button></td>
+          <td rowSpan={2} align="center" style={{ border: '2px solid black', width: '30px' }}><button type="button" className="btn btn-success"><h1><b>+</b></h1></button></td>
         </tr>
         
         {/* THIRD ROW */}
         <tr style={{ border: '2px solid black', height:'45px' }}>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>7</b></button></td>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>8</b></button></td>
-          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button><b>9</b></button></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button type="button" className="btn btn-outline-primary"><b>7</b></button></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button type="button" className="btn btn-outline-primary"><b>8</b></button></td>
+          <td align="center" style={{ border: '2px solid black' , width:'30px'}}><button type="button" className="btn btn-outline-primary"><b>9</b></button></td>
         </tr>
         
         {/* FOURTH ROW */}
         <tr style={{ border: '2px solid black', height:'45px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>4</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>5</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>6</b></button></td>
-          <td rowSpan={3} align="center" style={{ border: '2px solid black' }}><button><h1>=</h1></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>4</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>5</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>6</b></button></td>
+          <td rowSpan={3} align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-info"><h1><b>=</b></h1></button></td>
         </tr>
         
         {/* FIFTH ROW */}
         <tr style={{ border: '2px solid black', height:'45px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>1</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>2</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>3</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>1</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>2</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-outline-primary"><b>3</b></button></td>
         </tr>
         
         {/* SIXTH ROW */}
         <tr style={{ border: '2px solid black', height:'45px' }}>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>C</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>0</b></button></td>
-          <td align="center" style={{ border: '2px solid black' }}><button><b>Del</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-warning"><b>C</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-dark"><b>0</b></button></td>
+          <td align="center" style={{ border: '2px solid black' }}><button type="button" className="btn btn-danger"><b>Del</b></button></td>
         </tr>
 
       </table>
