86ec67698ebe08981bd9432f9fc0fc70347ad4a6
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-08 09:00:06 -0600

    Adding rows

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 function App() {
 
   return (
-     <div>
+     <div align="center">
       <h2>. . . CALCULATOR . . .</h2>
       <br />
-      {/* TABLE */}
-      <table style={{border:'2px solid black', width:'400px'}}>
-        {/* ROW */}
-        <tr>
-          <td align="center">RESULT</td>
+
+      {/* START TABLE */}
+      <table style={{ border: '5px solid black', width: '600px' }}>
+        {/* FIRST ROW */}
+        <tr style={{ border: '2px solid black'}}>
+          <td align="center">Fila 1</td>
         </tr>
-        {/* END ROW */}
-      </table>
-      {/* END TABLE */}
+        {/* SECOND ROW */}
+        <tr style={{ border: '2px solid black' } }>
+          <td align="center">Fila 2</td>
+        </tr>
+        {/* THIRD ROW */}
+        <tr style={{ border: '2px solid black' }}>
+          <td align="center">Fila 3</td>
+        </tr>
+        {/* FOURTH ROW */}
+        <tr style={{ border: '2px solid black' }}>
+          <td align="center">Fila 4</td>
+        </tr>
+        {/* FIFTH ROW */}
+        <tr style={{ border: '2px solid black' }}>
+          <td align="center">Fila 5</td>
+        </tr>
+        {/* SIXTH ROW */}
+        <tr style={{ border: '2px solid black' }}>
+          <td align="center">Fila 6</td>
+        </tr>
+      </table> {/* END TABLE */}
      </div>
   )
 }
