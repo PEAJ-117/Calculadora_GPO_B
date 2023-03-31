51ce8f02782f59118a8f1477a2496137eac42edf
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-03-31 12:30:14 -0600

    Button onClick to 7_8_9

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,43 @@ import { useState } from "react"
 import './App.css'
 
 function App() {
-  // Insertar variables  modificables
-  const [screen, setScreen] = useState("0");
-  // Funcion de los botones
-  const handleButtonClick = (e) => {
-    const value = e.target.value;
+  const [screen, setScreen] = useState("0");      // Insertar variables  modificables
+  
+  const handleButtonClickNine = () => {              // Funcion de los botones
     if (screen === '0') {
-      setScreen(value);
+      setScreen(`9`);
     } else {
-      setScreen(`${screen} ${value}`);
+      setScreen(`${screen}9`);
     }
   };
+
+  const handleButtonClickEight = () => {
+    if (screen === '0') {
+      setScreen(`8`);
+    } else {
+      setScreen(`${screen}8`);
+    }
+  };
+
+  const handleButtonClickSeven = () => {
+    if (screen === '0') {
+      setScreen(`7`);
+    } else {
+      setScreen(`${screen}7`);
+    }
+  }
 
   return (
      <div align="center">
       <h3>. . . CALCULATOR . . .</h3>
       <br />
       {/* START TABLE */}
-      <table style={{ border: '4px solid black', width: '200px' }}>
+      <table style={{ border: '4px solid black', width: '300px' }}>
         {/* FIRST ROW */}
-        <tr style={{ border: '4px solid black', height:'60px'}}>
+        <tr style={{ border: '4px solid black', height:'80px' }}>
           <td colSpan={4} align="right" style={{ border: '2px solid black' }}>
-            <h5>{screen}</h5> {/* Sustitui rpor variable */}
-            </td> {/* Column */}
+            <h1>{screen}</h1> {/* Imprime una variable */}
+          </td> {/* Column */}
         </tr>
 
         {/* SECOND ROW */}
@@ -38,9 +52,9 @@ function App() {
         
         {/* THIRD ROW */}
         <tr>
-          <td align="center"><button type="button" className="btn btn-outline-primary"><b>7</b></button></td>
-          <td align="center"><button type="button" className="btn btn-outline-primary"><b>8</b></button></td>
-          <td align="center"><button type="button" className="btn btn-outline-primary" handleButtonClick><b>9</b></button></td>
+          <td align="center"><button type="button" className="btn btn-outline-primary" onClick={handleButtonClickSeven}><b>7</b></button></td>
+          <td align="center"><button type="button" className="btn btn-outline-primary" onClick={handleButtonClickEight}><b>8</b></button></td>
+          <td align="center"><button type="button" className="btn btn-outline-primary" onClick={handleButtonClickNine}><b>9</b></button></td>
           <td align="center"><button type="button" className="btn btn-outline-secondary"><b>.</b></button></td>
         </tr>
         
